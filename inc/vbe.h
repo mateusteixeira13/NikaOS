b54732e45b9966822bb9af71d862334d44d5a50e
mateuscteixeira13 <maithaiw5@gmail.com>
2025-12-25 11:04:59 -0300

    Nika Kernel Update v0.52c3

--- inc/vbe.h
+++ inc/vbe.h
@@ -14,5 +14,34 @@ void draw_rect(uint32_t x, uint32_t y, uint32_t w, uint32_t h, uint32_t color);
 void clear(uint32_t color);
 GD_STATUS gd_init(multiboot_info_t* mbd, uint32_t magic);
 void gdl_print(const char *str);
+void gdl_putchar(char c);
+void gdl_print_color(const char *str, uint32_t color);
+
+/**
+ * Colors for VBE 32/24
+ */
+
+#define BLACK        0x000000
+#define WHITE        0xFFFFFF
+
+#define RED          0xFF0000
+#define GREEN        0x00FF00
+#define BLUE         0x0000FF
+
+#define YELLOW       0xFFFF00
+#define CYAN         0x00FFFF
+#define MAGENTA      0xFF00FF
+
+#define DARK_GRAY    0x404040
+#define GRAY         0x808080
+#define LIGHT_GRAY   0xC0C0C0
+
+#define ORANGE       0xFFA500
+#define BROWN        0x8B4513
+#define PINK         0xFFC0CB
+
+#define SKY_BLUE     0x87CEEB
+#define LIME         0x32CD32
+#define PURPLE       0x800080
 
 #endif
